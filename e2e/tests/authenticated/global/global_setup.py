6a54197257b3f65d3f3632d20aa22a668f9c77a4
Olena Kacher <o.kacher@pro.accompio.com>
2026-03-25 15:54:27 +0100

    remove unnecessary lines

--- e2e/tests/authenticated/global/global_setup.py
+++ e2e/tests/authenticated/global/global_setup.py
@@ -14,8 +14,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Import STORAGE_STATE from conftest
-# Add parent directory to path to import from conftest
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 from conftest import STORAGE_STATE
 
 
